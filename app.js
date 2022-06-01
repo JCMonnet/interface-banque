28444f3f230d40b92d0bedc3e74de15ec733ddee
JCMonnet <jcmonnet0408@gmail.com>
2022-06-01 13:12:33 +0200

    refactorisation

--- app.js
+++ app.js
@@ -22,67 +22,68 @@ function faireDepot(event) {
     // Empêcher la validation du formulaire d'actualiser la page
     event.preventDefault();
     let inputDepot = document.querySelector('#inputDepot');
-    let messageValidation=document.querySelector('.alert-depot');
+    let messageValidation = document.querySelector('.alert-depot');
     // Transformation de la valeur de l'input en nombre grâce à parseInt()
     let montantDepot = parseInt(inputDepot.value);
     // pour éviter l'enregistrement des depôts de 0 ou négatif
-    if (montantDepot<= 0){
-    messageValidation.textContent="désolé ce montant n'est pas valide";
-    messageValidation.classList.add('negative');
-}
-// pour éviter validation champs vide
-else if (inputDepot.value==""){
-    messageValidation.textContent="désolé vous devez saisir un montant";
-    messageValidation.classList.add('negative'); 
-}
+    if (montantDepot <= 0) {
+        messageValidation.textContent = "désolé ce montant n'est pas valide";
+        messageValidation.classList.add('negative');
+    }
+    // pour éviter validation champs vide
+    else if (inputDepot.value == "") {
+        messageValidation.textContent = "désolé vous devez saisir un montant";
+        messageValidation.classList.add('negative');
+    }
 
- else  {
-    compte.solde = compte.solde + montantDepot;
-    messageValidation.textContent="votre depôt a bien été enregistré";
-    messageValidation.classList.add('positive');
-    insererHistorique("depot", montantDepot);
-}
-actualiserCompte();
-    // On remet la value de l'input vide pour réinitialiser le champ
+    else {
+        compte.solde += montantDepot;
+        messageValidation.textContent = "votre depôt a bien été enregistré";
+        messageValidation.classList.remove("negative");
+        messageValidation.classList.add('positive');
+        insererHistorique("depot", montantDepot);
+        actualiserCompte();
+    }
     inputDepot.value = "";
-    
 };
 
 // cette fois avec fonction flechée
 let boutonRetrait = document.querySelector('#btn-retrait');
 boutonRetrait.addEventListener('click', faireRetrait => {
-faireRetrait.preventDefault();
-// Récupération de la valeur de l'input du formulaire
-let inputRetrait=document.querySelector('#inputRetrait');
-// Transformation de la valeur de l'input en nombre grâce à parseInt()
-let montantRetrait=parseInt(inputRetrait.value);
-// Récupération de l'endroit où placer le message d'erreur/validation
-let message=document.querySelector('.alert-retrait');
-// Si la valeur du retrait est plus grande que le solde actuel
-// Message d'erreur négatif
-// Ajout de la classe negative pour mettre le texte en rouge
-if (montantRetrait>compte.solde) {
-    message.textContent=" désolé, solde insuffisant";
-    message.classList.add("negative");
-}
-// pour éviter les retraits de 0 ou négatif
- else if(montantRetrait<=0){
-    message.textContent=" désolé, ce montant n'est pas valide";
-    message.classList.add("negative");
-}
-else if (inputRetrait.value==""){
-    message.textContent="désolé vous devez saisir un montant";
-    message.classList.add('negative') }
-// Sinon on effectue le retrait
-else {
-    compte.solde= compte.solde - montantRetrait
-    message.textContent=" votre retrait a bien été enregistré";
-    message.classList.add("positive");
-    insererHistorique("retrait", montantRetrait);
-};
-actualiserCompte();
-// On remet la value de l'input vide pour réinitialiser le champ
-inputRetrait.value="";
+    faireRetrait.preventDefault();
+    // Récupération de la valeur de l'input du formulaire
+    let inputRetrait = document.querySelector('#inputRetrait');
+    // Transformation de la valeur de l'input en nombre grâce à parseInt()
+    let montantRetrait = parseInt(inputRetrait.value);
+    // Récupération de l'endroit où placer le message d'erreur/validation
+    let message = document.querySelector('.alert-retrait');
+    // Si la valeur du retrait est plus grande que le solde actuel
+    // Message d'erreur négatif
+    // Ajout de la classe negative pour mettre le texte en rouge
+    if (montantRetrait > compte.solde) {
+        message.textContent = " désolé, solde insuffisant";
+        message.classList.add("negative");
+    }
+    // pour éviter les retraits de 0 ou négatif
+    else if (montantRetrait <= 0) {
+        message.textContent = " désolé, ce montant n'est pas valide";
+        message.classList.add("negative");
+    }
+    else if (inputRetrait.value == "") {
+        message.textContent = "désolé vous devez saisir un montant";
+        message.classList.add('negative')
+    }
+    // Sinon on effectue le retrait
+    else {
+        compte.solde -= montantRetrait;
+        message.textContent = " votre retrait a bien été enregistré";
+        message.classList.remove("negative");
+        message.classList.add("positive");
+        insererHistorique("retrait", montantRetrait);
+        actualiserCompte();
+    // On remet la value de l'input vide pour réinitialiser le champ
+    };
+    inputRetrait.value = "";
 });
 
 // Insertion de l'opération dans l'historique en fonction du type d'opération reçue
@@ -103,7 +104,7 @@ function insererHistorique(type, value) {
     let clone = document.importNode(template.content, true);
     // Modification du clone avec les informations qu'on veut lui donner
     clone.querySelector(".date").textContent = new Date().toDateString();
-    clone.querySelector(".value").textContent = value + " €";
+    clone.querySelector(".value").textContent = ' ' + value + " €";
 
     // Récupération de la div parente où importer le clone
     let historiqueListe = document.querySelector(".historique-list");
